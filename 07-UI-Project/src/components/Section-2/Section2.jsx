ba8c3765976b86ba058da220686d9d90bb820a08
Shaeeb Ali Khan <shaeebalikhan@gmail.com>
2026-07-16 12:05:56 +0530

    UI-Project-done

---
+++ 07-UI-Project/src/components/Section-2/Section2.jsx
@@ -0,0 +1,12 @@
+// Its work for full page 
+
+const Section2 = () => {
+    return(
+        <div className="h-screen  bg-amber-950">
+            Section2
+        </div>
+
+    )
+}
+
+export default Section2;
