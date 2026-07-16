961c41decbed395138157b9645697da4a585604f
Shaeeb Ali Khan <shaeebalikhan@gmail.com>
2026-07-16 11:04:25 +0530

    06-tailwind done

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Button from './components/button/Button'
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Button/>
+    </>
+  )
+}
+
+export default App
